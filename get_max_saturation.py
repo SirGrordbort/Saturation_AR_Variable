71e3235412ca2f1d5f10317544c24ccfcffc77dc
SirGrordbort <53068050+SirGrordbort@users.noreply.github.com>
2020-09-23 11:58:21 +1200

    finishing up tests for normivt

--- get_max_saturation.py
+++ get_max_saturation.py
@@ -5,13 +5,13 @@ import scipy.integrate as integrate
 import math
 
 def make_where(latitude, longitude, date):
-    where_string = "latitude = %d, longitude = %d, "%(latitude, longitude)
-    date_string = "date = " + str(date)
+    where_string = "latitude = %f AND longitude = %f AND " % (latitude, longitude)
+    date_string = "time = date '" + str(date)+"'"
     return where_string+date_string
 
 
 def make_dates(num_steps):
-    start = datetime.datetime(2018, 10, 1, 0)
+    start = datetime.datetime(2018, 10, 4, 0)
     date_list = [start - datetime.timedelta(hours=6*x) for x in range(num_steps)]
     return date_list
 
@@ -19,24 +19,39 @@ def make_dates(num_steps):
 def calc_max_q(temp):
     return 0.611*math.exp((17.27*temp)/(temp+237.3))
 
-temp_table = arcpy.GetParameter(0)
-out_table = arcpy.GetParameter(1)
-dates = make_dates(11)
-new_rows = arcpy.da.InsertCursor(out_table, ("latitude", "longitude", "date", "max_q"))
 
-arcpy.AddMessage("loaded tables and dates")
-for lat in np.arange(-70.0, 0, 0.25):
-    for lon in np.arange(90, 210, 0.25):
-        for date in dates:
-            where = make_where(lat, lon, date)
-            pressure_levels = arcpy.da.SearchCursor(temp_table, ("temperature", "pressure"), where_clause=where)
-            pressure = []  # assume sorted for now
-            max_q = []
-            for row in pressure_levels:
-                max_q.append(calc_max_q(row[0]))
-                pressure.append(row[1])
-            integrated_max_q = integrate.trapz(max_q, pressure * 100, axis=0)
-            new_rows.insert(lat, lon, date, integrated_max_q)
-arcpy.AddMessage("completed")
+try:
+
+    temp_table = arcpy.GetParameter(0)
+    mid_table = arcpy.GetParameter(1)
+    dates = [datetime.datetime(2018, 10, 4, 6), datetime.datetime(2018, 10, 3, 6)]  # make_dates(11) TODO reinstate after testing
+    new_rows = arcpy.da.InsertCursor(mid_table, ("latitude", "longitude", "time", "t"))
+
+    lats = [-70,-60,-50]# np.arange(-70.0, 0, 0.25) FIXME reinsert this code after testing
+    longs = [90,95,110]# np.arange(90, 210, 0.25)
+    for lat in lats:
+        for lon in longs:  # FIXME these are iterating with whole numbers still
+            for date in dates:
+                where = make_where(lat, lon, date)
+                arcpy.AddMessage(where)
+                pressure_levels = arcpy.da.SearchCursor(temp_table, ("t", "level"), where_clause=where)
+                pressure = []  # assume sorted for now
+                max_q = []
+                for row in pressure_levels:
+                    arcpy.AddMessage("iterated")
+                    max_q.append(calc_max_q(row[0]))
+                    pressure.append(row[1])
+
+                del pressure_levels
+                arcpy.AddMessage("pressures " + str(pressure))
+                arcpy.AddMessage("max_qs " + str(max_q))
+                if len(pressure) == 0 or len(max_q) == 0:
+                    continue
+                integrated_max_q = integrate.trapz(max_q, pressure, axis=0) # TODO had pressure times 1000 find out why
+                new_rows.insertRow((lat, lon, date, integrated_max_q))
+    arcpy.AddMessage("completed")
+
+finally:
+    del new_rows
 
 
--- get_saturation.py
+++ get_saturation.py
@@ -1,16 +1,32 @@
 import arcpy
-import make_point as mkpt
 import math
+
+def make_points(lon,lat, length, width):
+    points = arcpy.Array()
+    points.append(arcpy.Point(lat, lon))
+    points.append(arcpy.Point(lat+width, lon))
+    points.append(arcpy.Point(lat + width, lon + length))
+    points.append(arcpy.Point(lat, lon+length))
+    points.append(points[0])
+    return points
+
 table = arcpy.GetParameter(0)
 out_feature = arcpy.GetParameter(1)
-table_cursor = arcpy.da.SearchCursor(table, ("latitude", "longitude", "time", "IVT_N", "IVT_E", "qmax"))  # FIXME IVT needs to be calculated first
-add_polys = arcpy.da.InsertCursor(out_feature, ("time", "poly", "NORMIVT"))
+table_cursor = arcpy.da.SearchCursor(table, ("latitude", "longitude", "time", "IVT_N", "IVT_E", "max_q"))
+add_polys = arcpy.da.InsertCursor(out_feature, ("time", "Shape", "NORMIVT"))
 for lat_lon in table_cursor:
     lat = lat_lon[0]
     lon = lat_lon[1]
-    points = mkpt.make_points(lat, lon, 0.25, 0.25)
+    points = make_points(lat, lon, 0.25, 0.25)
+    arcpy.AddMessage(str(points))
     poly = arcpy.Polygon(points)
-    tot_IVT = math.hypot(lat_lon[4], lat_lon[5])
-    normIVT = tot_IVT/lat_lon[6]
-    add_polys.insertRow(lat_lon[3], poly, normIVT)
+    tot_IVT = math.hypot(lat_lon[3], lat_lon[4])
+    normIVT = float(tot_IVT)/float(lat_lon[5])
+    arcpy.AddMessage(str(lat_lon[3]))
+    arcpy.AddMessage(str(lat_lon[4]))
+    arcpy.AddMessage(str(lat_lon[5]))
+    arcpy.AddMessage(normIVT)
+    arcpy.AddMessage(poly.centroid)
+    add_polys.insertRow((lat_lon[2], poly, normIVT))
+    break;
 
